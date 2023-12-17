5674c05a2eb8431d6d8800afe26938eb24fc3e0c
b1lagaana <mastertrainpic@gmail.com>
2023-12-17 22:07:41 +0100

    AoC_2023_Day_07: first thoughts

--- 2023/Day07/main.py
+++ 2023/Day07/main.py
@@ -1,31 +1,7 @@
-file_data = "data.txt"
-file_data_02 = open("data.txt", "r")
-file_data_example_01 = "example_00.txt"
+play = []
 
-from collections import defaultdict
+for i in (open("example_00.txt", "r").read().splitlines()):
+    play.append((i.split(" ")[0], i.split(" ")[1]))
 
-card_list = {"A", "K", "Q", "J", "T", "9", "8", "7", "6", "5", "4", "3", "2"}
+# get rank of hand, multiply rank with bid for hand and add all set of hands winnings together
 
-
-fullhouse = False
-twopair = False
-
-for line in open(file_data_example_01):
-    value = ([line.split('\n')[0].split(' ')[0], line.split('\n')[0].split(' ')[1], ""])
-    print(value[0])
-    print(value[0].count("A"))
-    print(value[0].count("K"))
-    print(value[0].count("Q"))
-    print(value[0].count("J"))
-    print(value[0].count("T"))
-    print(value[0].count("9"))
-    print(value[0].count("8"))
-    print(value[0].count("7"))
-    print(value[0].count("6"))
-    print(value[0].count("5"))
-    print(value[0].count("4"))
-    print(value[0].count("3"))
-    print(value[0].count("2"))
-
-
-print("----------------")
