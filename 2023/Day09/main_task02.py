0149eb41c853e05ad82f72c0797702c5dd1710f0
b1lagaana <mastertrainpic@gmail.com>
2024-01-20 19:48:26 +0100

    AoC_2023_Day09: only with help!

---
+++ 2023/Day09/main_task02.py
@@ -0,0 +1,18 @@
+
+def extrapolate(array):
+    if all(x == 0 for x in array):
+        return 0
+
+    deltas = [y - x for x,y in zip(array, array[1:])]
+    diff = extrapolate(deltas)
+    return array[0] - diff
+
+total = 0
+
+for line in open("data.txt"):
+    history_sets = list(map(int, line.split()))
+
+    if history_sets:
+        total += extrapolate(history_sets)
+
+print(total)
